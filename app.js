fd270e25289baeb47e1b9b855a285975c2b8be47
nikolay7424 <nikolay7424@gmail.com>
2023-03-08 12:30:55 +0200

    actually done with header for desctop

--- app.js
+++ app.js
@@ -6,25 +6,29 @@ const catalogsElement = document.querySelector('.catalog');
 const subcatalogs = Array.from(document.querySelectorAll('.subcatalog'));
 
 catalogsElement.addEventListener('mouseover', e => {
+  const allActiveSubcatalogs = document.querySelectorAll('.active');
+  allActiveSubcatalogs.forEach(function(activeSubcatalog) {
+    activeSubcatalog.classList.remove('active');
+  });
   for(catalog in catalogs) {
     if(catalogs[catalog] === e.target) {
-      subcatalogs[catalog].style.display = 'block';
+      subcatalogs[catalog].classList.add('active');
+
+      const activeSubCatalog = document.querySelector('.active');
+      activeSubCatalog.addEventListener('mouseout', e => {
+        activeSubCatalog.classList.remove('active');
+      });
+      catalogs[catalog].addEventListener('mouseout', e => {
+        setTimeout(() => {
+          activeSubCatalog.classList.remove('active');
+        }, 100);
+      });
     }
-    subcatalogs[catalog].addEventListener('mouseover', e => {
-      subcatalogs[catalog].style.display = 'block';
-    });
-    subcatalogs[catalog].addEventListener('mouseout', e => {
-      subcatalogs[catalog].style.display = 'none';
-    });
   }
 });
 
-catalogsElement.addEventListener('mouseout', e => {
-  for(catalog in catalogs) {
-    if(catalogs[catalog] === e.target) {
-      subcatalogs[catalog].style.display = 'none';
-    }
-  }
-});
+
+
+
 
 
