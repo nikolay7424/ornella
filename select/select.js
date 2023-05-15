dfa5c8124267e22ec11611dd867408765edaa7ef
nikolay7424 <nikolay7424@gmail.com>
2023-05-15 23:35:57 +0300

    custom select day 1

---
+++ select/select.js
@@ -0,0 +1,54 @@
+
+export default class Select {
+  constructor(element) {
+    this.element = element;
+    this.options = getFormattedOptions(this.element.querySelectorAll('option'));
+    this.customElement = document.createElement('div');
+    this.labelElement = document.createElement('span');
+    this.optionsCustomElement = document.createElement('ul');
+    setupCustomElement(this);
+    element.style.display = 'none';
+    element.after(this.customElement);
+  }
+
+  get selectedOption() {
+    return this.options.find(option => option.selected);
+  }
+}
+
+function setupCustomElement(select) {
+  select.customElement.classList.add('custom-select-container');
+  select.customElement.tabIndex = 0;
+
+  select.labelElement.classList.add('custom-select-value');
+  select.labelElement.innerText = select.selectedOption.label;
+  select.customElement.append(select.labelElement);
+
+  select.optionsCustomElement.classList.add('custom-select-options');
+  select.options.forEach(option => {
+    const optionElement = document.createElement('li');
+    optionElement.classList.add('custom-select-option');
+    optionElement.classList.toggle('selected', option.selected);
+    optionElement.innerText = option.label;
+    optionElement.dataset.value = option.value;
+    select.optionsCustomElement.append(optionElement);
+  });
+  select.customElement.append(select.optionsCustomElement);
+
+  select.customElement.addEventListener('click', (e) => {
+    select.optionsCustomElement.classList.toggle('show');
+    // todo: change option
+  });
+  
+}
+
+function getFormattedOptions(optionElements) {
+  return [...optionElements].map(optionElement => {
+    return {
+      value: optionElement.value,
+      label: optionElement.label,
+      selected: optionElement.selected,
+      element: optionElement
+    }
+  });
+}
