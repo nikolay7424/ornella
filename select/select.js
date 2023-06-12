a006d76db404380dd9828468a85799f5e2927fc8
nikolay7424 <nikolay7424@gmail.com>
2023-06-12 18:37:51 +0300

    select main functionality

--- select/select.js
+++ select/select.js
@@ -14,6 +14,29 @@ export default class Select {
   get selectedOption() {
     return this.options.find(option => option.selected);
   }
+
+  get selectedOptionIndex() {
+    return this.options.indexOf(this.selectedOption);
+  }
+
+  selectValue(value) {
+    const newSelectedOption = this.options.find(option => option.value === value);
+    const prevSelectedOption = this.selectedOption;
+
+    prevSelectedOption.selected = false;
+    prevSelectedOption.element.selected = false;
+
+    newSelectedOption.selected = true;
+    newSelectedOption.element.selected = true;
+
+    this.labelElement.innerText = newSelectedOption.label;
+
+    // find option that is already selected and remove selected class
+    this.optionsCustomElement.querySelector(`[data-value = "${prevSelectedOption.value}"]`).classList.remove('selected');
+    const newCustomElement = this.optionsCustomElement.querySelector(`[data-value = "${newSelectedOption.value}"]`);
+    newCustomElement.classList.add('selected');
+    newCustomElement.scrollIntoView({'block': 'nearest'});
+  }
 }
 
 function setupCustomElement(select) {
@@ -26,21 +49,73 @@ function setupCustomElement(select) {
   select.customElement.append(select.labelElement);
 
   select.optionsCustomElement.classList.add('custom-select-options');
+  // li elements
   select.options.forEach(option => {
     const optionElement = document.createElement('li');
     optionElement.classList.add('custom-select-option');
     optionElement.classList.toggle('selected', option.selected);
     optionElement.innerText = option.label;
     optionElement.dataset.value = option.value;
+    optionElement.addEventListener('click', () => {
+      select.selectValue(option.value);
+      select.optionsCustomElement.classList.remove('show');
+    });
     select.optionsCustomElement.append(optionElement);
   });
   select.customElement.append(select.optionsCustomElement);
 
-  select.customElement.addEventListener('click', (e) => {
+  select.labelElement.addEventListener('click', (e) => {
     select.optionsCustomElement.classList.toggle('show');
-    // todo: change option
   });
-  
+
+  // hide when clicked outside
+  select.customElement.addEventListener('blur', () => {
+    select.optionsCustomElement.classList.remove('show');
+  });
+
+  // keyboard control
+  let debounceTimeout;
+  let searchTerm = '';
+  select.customElement.addEventListener('keydown', (e) => {
+    switch(e.code) {
+      case 'Space' :
+        case 'Enter':
+        select.optionsCustomElement.classList.toggle('show');
+        e.preventDefault();
+        break;
+      case 'ArrowUp' :
+        const prevOption = select.options[select.selectedOptionIndex - 1];
+        if(prevOption) {
+          select.selectValue(prevOption.value);
+        }
+        e.preventDefault();
+        break;
+      case 'ArrowDown' :
+        const nextOption = select.options[select.selectedOptionIndex + 1];
+        if(nextOption) {
+          select.selectValue(nextOption.value);
+        }
+        e.preventDefault();
+        break;
+      case 'Escape': 
+        select.optionsCustomElement.classList.remove('show');
+        e.preventDefault();
+        break;
+      default:
+        clearTimeout(debounceTimeout);
+        searchTerm += e.key;
+        debounceTimeout = setTimeout(() => {
+          searchTerm = '';
+        }, 500);
+        const searchOption = select.options.find(option => {
+          return option.label.toLowerCase().startsWith(searchTerm);
+        });
+        if(searchOption) {
+          select.selectValue(searchOption.value);
+        }
+
+    }
+  });
 }
 
 function getFormattedOptions(optionElements) {
